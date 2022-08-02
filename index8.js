f7eba84313e842b9cea56085fff8b55ce51f3656
Kateryna Tsygankova <kateryna.tsygankova.se@gmail.com>
2022-08-02 19:58:15 +0200

    3 new .js files added

---
+++ index8.js
@@ -0,0 +1,14 @@
+function calculateGuests(guestsInput) {
+  for (let symbol of guestsInput) {
+    console.log(typeof symbol);
+  }
+}
+
+calculateGuests("I think 5 guests");
+// calculateGuests("Big company of 15 dudes");
+// calculateGuests("5");
+// calculateGuests("alone");
+// calculateGuests("0");
+// calculateGuests("Hello, 9.75 people");
+// calculateGuests("There will be 7 or 9 guys");
+// calculateGuests("hello cat walks on my keyboard ksadjfhskaj12.34kasdfhsjk");
---
+++ index9.js
@@ -0,0 +1,13 @@
+function isAlphabet(letters) {
+  let alfabet = "abcdefghijklmnopqrstuvwxyz";
+}
+
+isAlphabet("abc");
+isAlphabet("aBc");
+isAlphabet("abd");
+isAlphabet("a");
+isAlphabet("");
+isAlphabet("abcdefghjiklmnopqrstuvwxyz");
+isAlphabet("tuvwxyz");
+isAlphabet("XYZ");
+isAlphabet("mnoprqst");
