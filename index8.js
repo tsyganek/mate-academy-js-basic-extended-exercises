81afcb174c542285c5d0bde1f8d97d72e5dbf8b6
Kateryna Tsygankova <kateryna.tsygankova.se@gmail.com>
2022-08-02 20:40:48 +0200

    index8.js task solved

--- index8.js
+++ index8.js
@@ -1,14 +1,22 @@
 function calculateGuests(guestsInput) {
-  for (let symbol of guestsInput) {
-    console.log(typeof symbol);
+  if (guestsInput.length === 0 || guestsInput === "0") {
+    return "not a number";
   }
+
+  const numberFound = guestsInput.match(/\d+/);
+
+  if (numberFound === null) {
+    return "not a number";
+  }
+
+  return Number(numberFound[0]);
 }
 
 calculateGuests("I think 5 guests");
-// calculateGuests("Big company of 15 dudes");
-// calculateGuests("5");
-// calculateGuests("alone");
-// calculateGuests("0");
-// calculateGuests("Hello, 9.75 people");
-// calculateGuests("There will be 7 or 9 guys");
-// calculateGuests("hello cat walks on my keyboard ksadjfhskaj12.34kasdfhsjk");
+calculateGuests("Big company of 15 dudes");
+calculateGuests("5");
+calculateGuests("alone");
+calculateGuests("0");
+calculateGuests("Hello, 9.75 people");
+calculateGuests("There will be 7 or 9 guys");
+calculateGuests("hello cat walks on my keyboard ksadjfhskaj12.34kasdfhsjk");
