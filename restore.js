0ff52279e8b0a5f137cda102919ebc2f5d28c3fc
Kateryna Tsygankova <kateryna.tsygankova.se@gmail.com>
2022-08-15 12:55:05 +0200

    restore tast solution is added

---
+++ restore.js
@@ -0,0 +1,37 @@
+function restoreNames(users) {
+  for (let user of users) {
+    if (users.firstName === undefined) {
+      const arr = user.fullName.split(" ");
+      user.firstName = arr[0];
+    }
+    console.log(users);
+  }
+}
+
+const users = [
+  {
+    firstName: undefined,
+    lastName: "Holy",
+    fullName: "Jack Holy",
+  },
+  {
+    lastName: "Adams",
+    fullName: "Mike Adams",
+  },
+];
+
+restoreNames(users);
+
+// users ===
+//   [
+//     {
+//       firstName: "Jack",
+//       lastName: "Holy",
+//       fullName: "Jack Holy",
+//     },
+//     {
+//       firstName: "Mike",
+//       lastName: "Adams",
+//       fullName: "Mike Adams",
+//     },
+//   ];
